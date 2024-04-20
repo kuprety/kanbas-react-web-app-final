de2ac06c930d33c807345366bb8ede844a51f71e
LalCelik <114354209+LalCelik@users.noreply.github.com>
2024-04-20 19:30:36 -0400

    k{

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -24,8 +24,10 @@ function Courses({ courses }: { courses: any[]; }) {
     const API_BASE = process.env.REACT_APP_BASE_API_URL;
 
     const { courseId } = useParams();
-    const COURSES_API = `${API_BASE}/api/courses`;
-    // const COURSES_API = "http://localhost:4000/api/courses";
+    //const COURSES_API = `${API_BASE}/api/courses`;
+
+     const COURSES_API = "http://localhost:4000/api/courses";
+
     const [course, setCourse] = useState<any>({ _id: "" });
     const findCourseById = async (courseId?: string) => {
       const response = await axios.get(
